80a3113f69b25c41e38857243ffdfdb3a32ffebb
Ahammed Unni <ahammedunnivp@gmail.com>
2023-12-05 01:43:09 +0530

    - UI de coupled and removed dependancies - Modified to call both micro-services

---
+++ src/TodoUI/Unni.ToDo.UI/DTOs/Todo/TodoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Unni.ToDo.UI.DTOs.Todo
+{
+    public class TodoDto
+    {
+        public int? Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsDone { get; set; } = false;
+        public int? Difficulty { get; set; }
+        public string? Category { get; set; }
+    }
+}
